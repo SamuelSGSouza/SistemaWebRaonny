38345388c7bfa2815a0ac84abb870e94daaee54b
SamuelSGSouza <samuels.g.desouza@gmail.com>
2026-03-03 16:11:28 -0300

    Deploy automático: atualização de sistema

--- core/views.py
+++ core/views.py
@@ -424,6 +424,10 @@ class ClienteUpdateView(LoginRequiredMixin,UpdateView):
     def get_context_data(self, **kwargs):
         ctx =  super().get_context_data(**kwargs)
         ctx["clientes_active"] = "active"
+
+        cliente = ctx["object"]
+        if cliente:
+            ctx["propostas"] = Proposta.objects.filter(cliente=cliente)
         return ctx
     
 @require_POST
